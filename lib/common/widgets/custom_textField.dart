7f11636502df3bc3ddc1e7e8ffcbe3ad11fe123d
Pushkar Bharuka <pushkargbharuka@gmail.com>
2025-12-16 19:53:54 +0530

    Add input validation to CustomTextField

--- lib/common/widgets/custom_textField.dart
+++ lib/common/widgets/custom_textField.dart
@@ -10,7 +10,10 @@ class CustomTextField extends StatelessWidget {
   Widget build(BuildContext context) {
     return TextFormField(
       validator: (val){
-
+        if(val==null || val == ''){
+          return "Enter your $hintText";
+        }
+        return null;
       },
       controller: controller,
       decoration: InputDecoration(
