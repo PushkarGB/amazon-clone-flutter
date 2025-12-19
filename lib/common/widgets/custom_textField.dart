6b33d62710afb06d67a8bc217f8f7ea2eaeacf3d
Pushkar Bharuka <pushkargbharuka@gmail.com>
2025-12-19 20:59:17 +0530

    refactor(ui): add configurable maxLines support to CustomTextField

--- lib/common/widgets/custom_textField.dart
+++ lib/common/widgets/custom_textField.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class CustomTextField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
-  const CustomTextField({super.key,required this.controller,required this.hintText});
+  final int maxLines;
+  const CustomTextField({super.key,required this.controller,required this.hintText,this.maxLines = 1});
 
 
   @override
@@ -15,6 +16,7 @@ class CustomTextField extends StatelessWidget {
         }
         return null;
       },
+      maxLines: maxLines,
       controller: controller,
       decoration: InputDecoration(
         hintText: hintText,
