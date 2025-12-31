0848858d6f682ccf3a6092aacb56e98b28a8fc8b
PushkarGB <pushkargbharuka@gmail.com>
2025-12-31 14:14:39 +0530

    feat(admin-ui): add GridView with loader for product listing

--- lib/features/admin/views/product_screen.dart
+++ lib/features/admin/views/product_screen.dart
@@ -1,4 +1,8 @@
+import 'package:amazon_clone/common/widgets/loader.dart';
+import 'package:amazon_clone/features/account/widgets/single_product.dart';
 import 'package:amazon_clone/features/admin/screens/add_product_screen.dart';
+import 'package:amazon_clone/features/admin/services/admin_services.dart';
+import 'package:amazon_clone/models/product.dart';
 import 'package:flutter/material.dart';
 
 class ProductScreen extends StatefulWidget {
@@ -9,18 +13,72 @@ class ProductScreen extends StatefulWidget {
 }
 
 class _ProductScreenState extends State<ProductScreen> {
+  List<Product>?
+  products; //kept nullable so that , we can check if it is null (not fetched) we show a loader and once it is non-null (fetch returns empty or list)
+  final adminServices = AdminServices();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchAllProducts();
+  }
+
+  fetchAllProducts() async {
+    //Created this separate function because we cannot use await in initState() , that is initState() function cannot be async
+    products = await adminServices.fetchAllProducts(context);
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const Center(child: Text("Product Screen")),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color.fromARGB(255, 125, 221, 216),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
-        tooltip: "Add a Product",
-        onPressed: () => Navigator.pushNamed(context, AddProductScreen.routeName),
-        child: Icon(Icons.add),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-    );
+    return products == null
+        ? const Loader()
+        : Scaffold(
+            body: GridView.builder(
+              itemCount: products!.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemBuilder: (context, item) {
+                var productData = products![item];
+                return Column(
+                  children: [
+                    SizedBox(
+                      height: 140,
+                      child: SingleProduct(image: productData.productImages[0]),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            productData.productName,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 2,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.delete_outline),
+                        ),
+                      ],
+                    ),
+                  ],
+                );
+              },
+            ),
+            floatingActionButton: FloatingActionButton(
+              backgroundColor: const Color.fromARGB(255, 125, 221, 216),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(40),
+              ),
+              tooltip: "Add a Product",
+              onPressed: () =>
+                  Navigator.pushNamed(context, AddProductScreen.routeName),
+              child: Icon(Icons.add),
+            ),
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+          );
   }
 }
