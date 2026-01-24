603f97d2cab917b2c3b3f127ad90fae56d3bb126
Pushkar Bharuka <pushkargbharuka@gmail.com>
2026-01-24 23:46:42 +0530

    Implement search API route on backend

--- server/routes/product.js
+++ server/routes/product.js
@@ -9,6 +9,23 @@ productRouter.get("/api/products", auth, async (req, res) => {
     console.log(req.query.category); // query is after '?' , so category is a prop in query
     const products = await Product.find({ category: req.query.category });
     res.json(products);
+  } catch (e) {``
+    res.status(500).json({ error: e.message });
+  }
+});
+
+//get request to search products and get them
+// call -> /api/products/search/(query ex-i) ==> access the query with req.param.name 
+productRouter.get("/api/products/search/:name", auth, async (req, res) => {
+  try {
+    console.log(req.params.name); // query is after '?' , so category is a prop in query
+    const products = await Product.find(
+      {
+      productName : { $regex: req.params.name, $options: "i"} 
+      }
+    );
+    console.log(products);
+    res.json(products);
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
